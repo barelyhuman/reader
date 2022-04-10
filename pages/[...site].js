20c93469170af2c0b4b50c6f366dab762b31539a
Reaper <ahoy@barelyhuman.dev>
2022-04-10 20:32:50 +0530

    init

---
+++ pages/[...site].js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { urlToHTML } from "~/lib/server/html";
+import { sanitizer } from "~/lib/client/sanitizer";
+
+export default function Home({ html }) {
+  const [sanitizedHTMl, setSanitizedHTML] = useState();
+
+  useEffect(() => {
+    sanitizer(html).then((x) => setSanitizedHTML(x));
+  }, [html]);
+
+  return (
+    <>
+      <section
+        className="wrapper"
+        dangerouslySetInnerHTML={{ __html: sanitizedHTMl }}
+      ></section>
+    </>
+  );
+}
+
+export async function getServerSideProps(ctx) {
+  const result = {
+    props: {},
+  };
+
+  if (ctx.params && ctx.params.site.length) {
+    const fullPath = "http://" + ctx.params.site.join("/");
+    result.props.html = await urlToHTML(fullPath);
+  }
+
+  return result;
+}
